b27847cac55fb25840e64690b80a2641670a85aa
daseulkim128 <rosiekim128@gmail.com>
2021-06-08 14:20:00 +0900

    REDIS config 추가

---
+++ demo/src/main/java/com/example/demo/webConfig/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.webConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+	
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+		return lettuceConnectionFactory;
+	}
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		return redisTemplate;
+	}
+}
